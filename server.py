89c416832683f4f8f9b308ad88552261d2eacb32
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-10-20 00:47:14 +0300

    New events

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from config import *
 authorize = vk_api.VkApi(token = main_token)
 upload = vk_api.VkUpload(authorize)
 longpoll = VkBotLongPoll(authorize, group_id)
-
+log = methods.event_logs
 ### Методы для общения с ВК
 async def write_msg(sender, message):
     authorize.method('messages.send', {'chat_id': sender, 'message': message, 'random_id': 0})
@@ -21,36 +21,52 @@ async def main():
         try:
             await event_handle(event)
         except:
-            print("Error_log: [Handle error]")
+            methods.event_logs('Server_error', '5XX')
 
 ### Основная логика тут, в том числе обработка ивентов
-async def event_handle(event):    
-    if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != "":
-        
+async def event_handle(event):
+    try:
+
+        ### Часто использующиеся параметры
         msg = event.message.get('text').lower()
         words = event.message.get('text').lower().split()
         chat = event.chat_id
-        user_id = event.message.get('from_id')
+        user_id = event.message.get('from_id')    
 
-        ### Логи
-        print('Chat_id: [' + str(chat) + ']\nUser_id: [' + str(user_id) + ']\nMessage: [' + msg + ']')
+        if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != "":
+            
+            await log('New_message', msg, user_id)
 
-        if msg == '/гороскоп':
-            await write_msg(chat, 'Укажите знак зодиака 👺')
-        elif words[0] == '/гороскоп':
-            if words[1] in zodiac_signs:
-                photo = upload.photo_messages('Ваш путь к фото')
-                attachment = "photo" + str(photo[0]['owner_id']) + "_" + str(photo[0]['id']) + "_" + str(photo[0]['access_key'])
-                await send_picture(chat, methods.parse_horoscope(words[1]), attachment)
-            else:
-                await write_msg(chat, 'Моими лапами невозможно найти подобный знак зодиака 😿') 
+            if msg == '/гороскоп':
+                await write_msg(chat, 'Укажите знак зодиака 👺')
+            elif words[0] == '/гороскоп':
+                if words[1] in zodiac_signs:
+                    photo = upload.photo_messages('uploads/Кот_' + words[1] + '.png')
+                    attachment = "photo" + str(photo[0]['owner_id']) + "_" + str(photo[0]['id']) + "_" + str(photo[0]['access_key'])
+                    await send_picture(chat, methods.parse_horoscope(words[1]), attachment)
+                else:
+                    await write_msg(chat, 'Моими лапами невозможно найти подобный знак зодиака 😿') 
+
+            if words[0] == '/расписание':
+                if msg == words[0] or len(words) == 2:
+                    await write_msg(chat, 'Укажите КУРС и ГРУППУ!')
+                elif len(words) > 3:
+                    await write_msg(chat, methods.parse_schedule(words[1], words[2], words[3]))
+                else:
+                    await write_msg(chat, methods.parse_schedule(words[1], words[2]))
     
-        if words[0] == '/расписание':
-            if msg == words[0] or len(words) == 2:
-                await write_msg(chat, 'Укажите КУРС и ГРУППУ!')
-            elif len(words) > 3:
-                await write_msg(chat, methods.parse_schedule(words[1], words[2], words[3]))
-            else:
-                await write_msg(chat, methods.parse_schedule(words[1], words[2]))
+        elif event.type == VkBotEventType.MESSAGE_NEW and (event.message.action.get('type') == 'chat_invite_user' or event.message.action.get('type') == 'chat_invite_user_by_link'):
+            member_id = event.message.action.get('member_id')
+
+            await log('New_user', member_id)
+            await write_msg(event.chat_id, f"@id{member_id} (Странник), привет, какими судьбами?")
+    
+        elif event.type == VkBotEventType.MESSAGE_NEW and event.message.action.get('type') == 'chat_kick_user':
+            member_id = event.message.action.get('member_id')
+
+            await log('Kick_user', member_id)
+            await write_msg(event.chat_id, f"@id{user_id} (Человек) отправил в далекое плавание @id{member_id} (человека)\nPress F")
+    except:
+        await methods.event_logs('Handle_error', 'undefiend event')
 
 asyncio.run(main())

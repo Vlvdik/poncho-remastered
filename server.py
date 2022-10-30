de603518ff45dc81fc2f6cbffa020c084860a0e4
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-10-30 17:05:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,13 +5,16 @@ import handlers
 from vk_api.bot_longpoll import VkBotEventType
 from config import chats_limit, bot_id
 
+logging.basicConfig(level=logging.INFO, filename='Ваш путь к логам')
+log = logging.getLogger('SERVER.PY')
+
 ### Вхождение в луп
 async def main():
     for event in handlers.longpoll.listen():
         try:
             await asyncio.gather(event_handle(event))
         except:
-            pass
+            logging.critical('SERVER ERROR: breaks in the program logic')
 
 ### Основная логика тут, в том числе обработка ивентов
 async def event_handle(event):
@@ -24,6 +27,8 @@ async def event_handle(event):
         user_id = event.message.get('from_id')    
 
         if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != "":
+            
+            log.info(f'\nNEW MESSAGE: {msg} \nFROM CHAT: {chat_id} \nFROM USER: {user_id}\n')
 
             if msg == '/help':
                 await handlers.help(chat_id)
@@ -53,24 +58,29 @@ async def event_handle(event):
                 try:
                     await handlers.check_chat_limit(chat_id, user_id)
                 except:
-                    pass
+                    log.warning(f'\nRIGHTS ERROR: attempt to kick administrator (CHAT: {chat_id})\n')
     
         elif event.type == VkBotEventType.MESSAGE_NEW and (event.message.action.get('type') == 'chat_invite_user' or event.message.action.get('type') == 'chat_invite_user_by_link'):
             member_id = event.message.action.get('member_id')
             
             if member_id == bot_id:
+                log.info(f'\nNEW CHAT: {chat_id}\n')
                 await handlers.chat_greeting(chat_id)
             else:
+                log.info(f'\nNEW USER: {member_id} (CHAT: {chat_id})\n')
                 await handlers.user_greeting(chat_id, member_id)
     
         elif event.type == VkBotEventType.MESSAGE_NEW and event.message.action.get('type') == 'chat_kick_user':
             member_id = event.message.action.get('member_id')
         
             if user_id == member_id:
+                log.info(f'\nLEAVE USER: {member_id} (CHAT: {chat_id})\n')
                 await handlers.leave_user(chat_id, member_id)                
             else:    
+                log.info(f'\nKICK USER: {member_id} (CHAT: {chat_id})\n')
                 await handlers.kick_user(chat_id, user_id, member_id)
     except:
-        pass
+        log.warning(f'\nHANDLE ERROR: undefiend event (CHAT: {chat_id})\n')
+        
 
 asyncio.run(main())

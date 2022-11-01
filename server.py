ee090fbddf0c6e5aaa72c38d21779a53baeb420e
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-11-01 23:19:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -19,85 +19,88 @@ async def main():
 ### Основная логика тут, в том числе обработка ивентов
 async def event_handle(event):
     try:
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            ### Часто использующиеся параметры
+            msg = event.message.get('text').lower()
+            words = msg.split()
+            user_id = event.message.get('from_id')  
+            if event.from_user and event.message.get('text') != "":
 
-        ### Часто использующиеся параметры
-        msg = event.message.get('text').lower()
-        words = msg.split()
-        chat_id = event.chat_id
-        user_id = event.message.get('from_id')    
+                log.info(f'\nNEW DIRECT MESSAGE: {msg} FROM USER: {user_id}\n')
 
-        if event.type == VkBotEventType.MESSAGE_NEW and event.from_user and event.message.get('text') != "":
-            
-            log.info(f'\nNEW DIRECT MESSAGE: {msg} FROM USER: {user_id}\n')
-
-            if msg == 'начать' and  user_id not in users_group:
-                await handlers.start(user_id)
-            elif msg in forms:
-                await handlers.set_form(user_id, msg)
-            elif msg == 'назад' and user_id in users_group:
-                await handlers.back(user_id)
-            elif user_id in users_group:
-                if 'Группа' in users_group[user_id]:
-                    await handlers.push_button(user_id, msg)
+                if msg == 'начать' and  user_id not in users_group:
+                    await handlers.start(user_id)
+                elif msg in forms:
+                    await handlers.set_form(user_id, msg)
+                elif msg == 'назад' and user_id in users_group:
+                    await handlers.back(user_id)
+                elif user_id in users_group:
+                    if 'Группа' in users_group[user_id]:
+                        await handlers.push_button(user_id, msg)
+                    else:
+                        await handlers.set_group(user_id, msg)
                 else:
-                    await handlers.set_group(user_id, msg)
-            else:
-                await handlers.write_msg(user_id, 'Такая команда недоступна, попробуй "Начать"')
+                    await handlers.starter(user_id)
 
-        elif event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != "":
-    
-            log.info(f'\nNEW MESSAGE: {msg} \nFROM CHAT: {chat_id} \nFROM USER: {user_id}\n')
+            elif event.from_chat and event.message.get('text') != "":
+                chat_id = event.chat_id
 
-            if msg == '/help':
-                await handlers.help(chat_id)
+                log.info(f'\nNEW MESSAGE: {msg} \nFROM CHAT: {chat_id} \nFROM USER: {user_id}\n')
 
-            if msg == '/bibametr':
-                await handlers.bibametr(chat_id, user_id)
+                if msg == '/help':
+                    await handlers.help(chat_id)
 
-            if msg == '/быдло':
-                await handlers.get_chat_info(chat_id)
+                if msg == '/bibametr':
+                    await handlers.bibametr(chat_id, user_id)
 
-            if msg == '/рулетка':
-                await handlers.roulette(chat_id, user_id)
+                if msg == '/быдло':
+                    await handlers.get_chat_info(chat_id)
 
-            if words[0] == '/лимит':
-                await handlers.set_chat_limit(chat_id, words)
+                if msg == '/рулетка':
+                    await handlers.roulette(chat_id, user_id)
 
-            if words[0] == '/гороскоп':
-                await handlers.horoscope(chat_id, words)
+                if words[0] == '/лимит':
+                    await handlers.set_chat_limit(chat_id, words)
 
-            if words[0] == '/расписание':
-                await handlers.schedule(chat_id, words)
+                if words[0] == '/гороскоп':
+                    await handlers.horoscope(chat_id, words)
 
-            ### Обновлеям токсичность чата
-            await methods.refresh_chats_info(chat_id, user_id, msg)
+                if words[0] == '/расписание':
+                    await handlers.schedule(chat_id, words)
+
+                ### Обновлеям токсичность чата
+                await methods.refresh_chats_info(chat_id, user_id, msg)
             
-            if chat_id in chats_limit:
-                try:
-                    await handlers.check_chat_limit(chat_id, user_id)
-                except:
-                    log.warning(f'\nRIGHTS ERROR: attempt to kick administrator (CHAT: {chat_id})\n')
+                if chat_id in chats_limit:
+                    try:
+                        await handlers.check_chat_limit(chat_id, user_id)
+                    except:
+                        log.warning(f'\nRIGHTS ERROR: attempt to kick administrator (CHAT: {chat_id})\n')
     
-        elif event.type == VkBotEventType.MESSAGE_NEW and (event.message.action.get('type') == 'chat_invite_user' or event.message.action.get('type') == 'chat_invite_user_by_link'):
-            member_id = event.message.action.get('member_id')
+            elif event.message.action.get('type') == 'chat_invite_user' or event.message.action.get('type') == 'chat_invite_user_by_link':
+                member_id = event.message.action.get('member_id')
+                chat_id = event.chat_id
+
             
-            if member_id == bot_id:
-                log.info(f'\nNEW CHAT: {chat_id}\n')
-                await handlers.chat_greeting(chat_id)
-            else:
-                log.info(f'\nNEW USER: {member_id} (CHAT: {chat_id})\n')
-                await handlers.user_greeting(chat_id, member_id)
+                if member_id == bot_id:
+                    log.info(f'\nNEW CHAT: {chat_id}\n')
+                    await handlers.chat_greeting(chat_id)
+                else:
+                    log.info(f'\nNEW USER: {member_id} (CHAT: {chat_id})\n')
+                    await handlers.user_greeting(chat_id, member_id)
     
-        elif event.type == VkBotEventType.MESSAGE_NEW and event.message.action.get('type') == 'chat_kick_user':
-            member_id = event.message.action.get('member_id')
-        
-            if user_id == member_id:
-                log.info(f'\nLEAVE USER: {member_id} (CHAT: {chat_id})\n')
-                await handlers.leave_user(chat_id, member_id)                
-            else:    
-                log.info(f'\nKICK USER: {member_id} (CHAT: {chat_id})\n')
-                await handlers.kick_user(chat_id, user_id, member_id)
+            elif event.message.action.get('type') == 'chat_kick_user':
+                member_id = event.message.action.get('member_id')
+                chat_id = event.chat_id
+
+                if user_id == member_id:
+                    log.info(f'\nLEAVE USER: {member_id} (CHAT: {chat_id})\n')
+                    await handlers.leave_user(chat_id, member_id)                
+                else:    
+                    log.info(f'\nKICK USER: {member_id} (CHAT: {chat_id})\n')
+                    await handlers.kick_user(chat_id, user_id, member_id)
     except:
         log.warning(f'\nHANDLE ERROR: undefiend event (CHAT: {chat_id})\n')
         
+
 asyncio.run(main())

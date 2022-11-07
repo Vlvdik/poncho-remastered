a0d9ba56d440b8829a1a8a7a49e8559b41188b6f
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-11-07 20:37:35 +0300

    Update db_methods.py

--- db_methods.py
+++ db_methods.py
@@ -1,5 +1,7 @@
 from config import *
 
+
+###### METHODS FOR SHEDULES TABLE
 ### CREATE/UPDATE
 
 async def insert_user(user_id):
@@ -51,4 +53,63 @@ async def is_user_have_group(user_id):
 
 async def delete_user(user_id):
     cursor.execute('DELETE FROM schedules WHERE user_id=?', (user_id, ))
+    db.commit()
+
+###### METHODS FOR CHATS TABLE
+### CREATE/UPDATE
+
+async def insert_chats_limit(chat_id, value):
+    cursor.execute('INSERT INTO chats VALUES(?,?)', (chat_id, value))
+    db.commit()
+
+async def update_chats_limit(chat_id, value):
+    cursor.execute('UPDATE chats SET chats_limit=? WHERE user_id=?;', (value, chat_id))
+    db.commit()
+
+### READ
+
+async def is_chat_have_limit(chat_id):
+    if cursor.execute('SELECT chat_id FROM chats WHERE chat_id=?;', (chat_id, )).fetchone() != None:
+        return True
+    else:
+        return False
+
+### DELETE
+
+async def delete_chats_limit(chat_id):
+    cursor.execute('DELETE FROM chats WHERE chat_id=?', (chat_id, ))
+    db.commit()
+
+###### METHODS FOR USERS TABLE
+### CREATE/UPDATE
+
+async def insert_chat_user_score(user_id, chat_id, score):
+    cursor.execute('INSERT INTO users VALUES(?,?,?)', (user_id, chat_id, score))
+    db.commit()
+
+async def update_chat_user_score(user_id, chat_id, score):
+    cursor.execute('UPDATE users SET score=score+? WHERE user_id=? AND users_chat=?', (score, user_id, chat_id))
+    db.commit()
+
+### READ
+
+async def get_score_info(chat_id):
+    return cursor.execute('SELECT user_id, score FROM users WHERE users_chat=? AND score>0 ORDER BY score DESC;', (chat_id, )).fetchall()
+
+async def is_existed_chat(chat_id):
+    if cursor.execute('SELECT users_chat FROM users WHERE users_chat=?;', (chat_id, )).fetchone() != None:
+        return True
+    else:
+        return False
+
+async def is_user_exist_in_current_chat(user_id, chat_id):
+    if cursor.execute('SELECT user_id FROM users WHERE user_id=? AND users_chat=?;', (user_id, chat_id)).fetchone() != None:
+        return True
+    else:
+        return False
+
+### DELETE
+
+async def delete_user_from_chat(user_id, chat_id):
+    cursor.execute('DELETE FROM users WHERE user_id=? AND users_chat=?', (user_id, chat_id))
     db.commit()
